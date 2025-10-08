1c6b1f3933cf3df2b93b3d402d799e771f75d917
EddyRes <eresen03@calpoly.edu>
2025-10-07 18:22:54 -0700

    feat(frontend): POST /users on submit and update table on success

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
@@ -13,11 +13,44 @@ function MyApp() {
   function removeOneCharacter(index) {
     setCharacters(characters.filter((_, i) => i !== index));
   }
-
-  // exactly as spec
+  
   function updateList(person) {
-    setCharacters([...characters, person]);
+    postUser(person)
+      .then(() => {
+        setCharacters([...characters, person]);
+      })
+      .catch((err) => {
+        console.log("POST /users failed:", err);
+      });
   }
+
+  function fetchUsers() {
+  return fetch("http://localhost:8000/users"); // returns a Promise
+  }
+
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((err) => console.log(err));
+  }, []);
+
+  function postUser(person) {
+  return fetch("http://localhost:8000/users", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(person),
+  });
+  }
+  function postUser(person) {
+  return fetch("http://localhost:8000/users", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(person),
+  });
+  }
+
+
 
   return (
     <div className="container">
