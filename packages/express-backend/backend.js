1c792997889c1c33a1e9f25bdfa399a4beb6f524
EddyRes <eresen03@calpoly.edu>
2025-10-07 18:04:23 -0700

    (backend): enable CORS for frontend requests

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,10 +1,12 @@
 // packages/express-backend/backend.js
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
 
 // Parse JSON bodies
+app.use(cors());
 app.use(express.json());
 
 // ---- In-memory data ----
